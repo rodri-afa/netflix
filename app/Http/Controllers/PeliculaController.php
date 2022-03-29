0e9d891b213fed4d4eb2ccbee28418bc54e7ed0d
rodri-afa <rodrigo.afaformacion@gmail.com>
2022-03-29 09:17:19 +0200

    all pelis

---
+++ app/Http/Controllers/PeliculaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePeliculaRequest;
+use App\Http\Requests\UpdatePeliculaRequest;
+use App\Models\Pelicula;
+
+class PeliculaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pelis = Pelicula::all();
+        echo $pelis;
+        // return view('pelis');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePeliculaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePeliculaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pelicula $pelicula)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pelicula $pelicula)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePeliculaRequest  $request
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePeliculaRequest $request, Pelicula $pelicula)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pelicula $pelicula)
+    {
+        //
+    }
+}
